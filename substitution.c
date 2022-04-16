c3017fd7e2656afafd4af71a900fdc1b1d8379cb
MAHIN0093 <101042038+MAHIN0093@users.noreply.github.com>
2022-04-16 07:09:10 +0600

    Update substitution.c

--- substitution.c
+++ substitution.c
@@ -3,11 +3,11 @@
 #include <ctype.h>
 #include <string.h>
 
-bool is_unique(string c);
+bool is_not_unique(string c);
+void cipher(char pos,string key);
 
 int main(int argc, string argv[])
 {
-    string key = argv[1];
     if (argc != 2)
     {
         printf("USAGE: ./substitution KEY\n");
@@ -15,19 +15,28 @@ int main(int argc, string argv[])
     }
     else
     {
-        if (strlen(key) != 26)
+        if (strlen(argv[1]) != 26) //counting all characters;
         {
             printf("KEY must contain 26 characters!\n");
             return 1;
         }
         else
         {
-            if (isalpha(key) != 26)
+            int alpha_count = 0;
+            for (int i = 0; i < 26; i++)
+            {
+                if (isalpha(argv[1][i]))
+                {
+                    alpha_count++; //counting only alphabets;
+                }
+
+            }
+            if (alpha_count != 26)
             {
                 printf("KEY must contain only alphabatical characters!\n");
                 return 1;
             }
-            else if(is_unique(key) == false)
+            else if(is_not_unique(argv[1]))
             {
                 printf("KEY must only contain unique characters!\n");
                 return 1;
@@ -35,8 +44,26 @@ int main(int argc, string argv[])
             else
             {
                 string plaintext = get_string("plaintext: ");
-
-                printf("%s\n",plaintext);
+                printf("ciphertext:");
+                for( int i = 0; i < strlen(plaintext); i++)
+                {
+                    if (isalpha(plaintext[i]))
+                    {
+                        if(islower(plaintext[i]))
+                        {
+                            cipher(tolower(plaintext[i]));
+                        }
+                        else
+                        {
+                            cipher(toupper(plaintext[i]));
+                        }
+                    }
+                    else
+                    {
+                        printf("%c",plaintext[i]);
+                    }
+                }
+                printf("\n");
             }
         }
 
@@ -44,7 +71,7 @@ int main(int argc, string argv[])
 
 }
 
-bool is_unique(string c)
+bool is_not_unique(string c)
 {
     int n = strlen(c);
     int i, j = 1;
@@ -54,9 +81,27 @@ bool is_unique(string c)
         {
             if (c[i] == c[j])    //Check for duplicate elements
             {
-                return false;
+                return true;
             }
         }
     }
-return true;
+    return false;
+}
+
+void cipher(char pos,string key)
+{
+    string alpha_array = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (int i = 0; i < strlen(alpha_array); i++)
+    {
+        if (islower(pos))
+        {
+            if(pos == islower(alpha_array[i]))
+            printf("%c",tolower(key[i]));
+        }
+        else if (isupper(pos))
+        {
+            if(pos == isupper(alpha_array[i]))
+            printf("%c",toupper(key[i]));
+        }
+    }
 }
