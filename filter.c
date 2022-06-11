e6eed5e5045322fb60b251a80d4b0060fa39f178
MAHIN0093 <101042038+MAHIN0093@users.noreply.github.com>
2022-06-11 12:25:28 +0600

    Update filter.c

--- filter.c
+++ filter.c
@@ -1,52 +1,46 @@
-#include "helpers.h"
-#include <math.h>
-
-// Convert image to grayscale
-void grayscale(int height, int width, RGBTRIPLE image[height][width])
-{
-    for ( int i = 0; i < height-1; i++)
-    {
-        for ( int j = 0; j < width-1; j++)
-        {
-            float average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3);
-            image[i][j].rgbtBlue = average;
-            image[i][j].rgbtGreen = average;
-            image[i][j].rgbtRed = average;
-        }
-    }
-    return;
-}
-
-// Reflect image horizontally
-void reflect(int height, int width, RGBTRIPLE image[height][width])
-{
-    int color[3];
-    for ( int i = 0; i < height-1; i++)
-    {
-        for ( int j = 0; j < (width-1)/2; j++)
-        {
-            color[0] = image[i][j].rgbtRed;
-            color[1] = image[i][j].rgbtBlue;
-            color[2] = image[i][j].rgbtGreen;
-            image[i][j].rgbtRed = image[i][width-1-j].rgbtRed;
-            image[i][j].rgbtBlue = image[i][width-1-j].rgbtBlue;
-            image[i][j].rgbtGreen = image[i][width-1-j].rgbtGreen;
-            image[i][width-1-j].rgbtRed = color[0];
-            image[i][width-1-j].rgbtBlue = color[1];
-            image[i][width-1-j].rgbtGreen = color[2];
-        }
-    }
-    return;
-}
-
-// Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
-}
-
-// Detect edges
-void edges(int height, int width, RGBTRIPLE image[height][width])
-{
+        RGBTRIPLE temp[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            temp[i][j] = image[i][j];
+        }
+    }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int count = 0;
+            double sum_blue = 0;
+            double sum_green = 0;
+            double sum_red = 0;
+            for (int k = - 1; k < 2; k++)
+            {
+                for (int l = - 1; l < 2; l++)
+                {
+                    if ((k + i) < 0 || (l + j) < 0 || (k + i) > (height-1) || (l + j) < (width-1))
+                    {
+                        continue;
+                    }
+                    sum_blue += temp[k+i][l+j].rgbtBlue;
+                    sum_green += temp[k+i][l+j].rgbtGreen;
+                    sum_red += temp[k+i][l+j].rgbtRed;
+                    count++;
+                }
+            }
+            temp[i][j].rgbtBlue = round(sum_blue / count);
+            temp[i][j].rgbtGreen = round(sum_green / count);
+            temp[i][j].rgbtRed = round(sum_red / count);
+        }
+    }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = temp[i][j];
+        }
+    }
     return;
 }
